c47cfe3b438b5c503bc8430867413778ea21eec7
Enoch2k2 <enoch2k2@hotmail.com>
2023-05-01 12:23:34 -0500

    finished

---
+++ lib/migrations/versions/747d88b666bb_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 747d88b666bb
+Revises: 
+Create Date: 2023-05-01 12:14:35.378073
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '747d88b666bb'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
